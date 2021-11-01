02e5428b405813e2be316d327fab65a3437c83cb
Gyu-Park <kyesung8282@gmail.com>
2021-11-01 15:38:27 -0400

    Spring Boot Maven Project

---
+++ src/main/java/com/groupproject/boogle/model/User.java
@@ -0,0 +1,93 @@
+package com.groupproject.boogle.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long userid;
+	
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+	
+	@Column(nullable = false, length = 64)
+	private String password;
+	
+	@Column(length = 20)
+	private String firstname;
+	
+	@Column(length = 20)
+	private String lastname;
+	
+	public Long getUserid() {
+		return userid;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, firstname, lastname, password, userid);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(firstname, other.firstname)
+				&& Objects.equals(lastname, other.lastname) && Objects.equals(password, other.password)
+				&& Objects.equals(userid, other.userid);
+	}
+
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", email=" + email + ", password=" + password + ", firstname=" + firstname
+				+ ", lastname=" + lastname + "]";
+	}
+	
+}
