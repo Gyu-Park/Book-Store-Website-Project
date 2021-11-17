74a871d000655d6a7c54b3e3557296538c56739d
SimplyFlawed <72576623+SimplyFlawed@users.noreply.github.com>
2021-11-16 23:55:42 -0500

    updated nav.js for cart qty function

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector("#navToggle");
 const nav = document.querySelector("nav");
 const navIcon = document.querySelectorAll(".nav-toggle .fas");
+const cartNumP = document.querySelector(".cart-num p");
+const cartNumDiv = document.querySelector(".cart-num");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
@@ -8,3 +10,9 @@ navToggle.addEventListener("click", () => {
     icon.classList.toggle("nav-hidden");
   });
 });
+
+if (cartNumP.innerHTML == "0") {
+  cartNumDiv.classList.add("nav-hidden");
+} else if (cartNumP.innerHTML > 0) {
+  cartNumDiv.classList.remove("nav-hidden");
+}
