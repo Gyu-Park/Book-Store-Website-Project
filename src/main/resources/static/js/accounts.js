45005128e74df8d81cff632304efcf38b405cdaf
Gyu-Park <kyesung8282@gmail.com>
2021-11-30 16:45:20 -0500

    Bug fix and Add change-password function

--- src/main/resources/static/js/accounts.js
+++ src/main/resources/static/js/accounts.js
@@ -27,6 +27,80 @@ function openForm(evt, formName) {
     evt.currentTarget.className += " active";
 }
 
+/*Login Security Edit*/
+
+function onPhoneEditButtonClick(){
+	if (document.getElementById('edit-phone').className === "hide") {
+		document.getElementById('edit-phone').className = "show";
+  		document.getElementById('edit-phone-button').className = "show";
+	} else if (document.getElementById('edit-phone').className === "show") {
+		document.getElementById('edit-phone').className = "hide";
+  		document.getElementById('edit-phone-button').className = "hide";
+	}
+}
+
+function onPasswordEditButtonClick(){
+	if (document.getElementById('edit-password').className === "form-control-hide" 
+		|| document.getElementById('edit-password').className === "form-control-hide success"
+		|| document.getElementById('edit-password').className === "form-control-hide error") {
+		document.getElementById('edit-password').className = "form-control-show";
+		document.getElementById('edit-repeat-password').className = "form-control-show";
+  		document.getElementById('edit-password-button').className = "form-control-show";
+	} else if (document.getElementById('edit-password').className === "form-control-show"
+		|| document.getElementById('edit-password').className === "form-control-show success"
+		|| document.getElementById('edit-password').className === "form-control-show error") {
+		document.getElementById('edit-password').className = "form-control-hide";
+		document.getElementById('edit-repeat-password').className = "form-control-hide";
+  		document.getElementById('edit-password-button').className = "form-control-hide";
+	}
+}
+
+/*Login Secutity tab - Password Validation*/
+const password = document.querySelector('.edit-password');
+const password2 = document.querySelector('.edit-repeat-password');
+
+function checkInputs() {
+
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+        return false;
+	} else if(passwordValue.length < 8) {
+        setErrorFor(password, 'Password must be at least 8 characters');
+        return false;
+    } else if(passwordValue.length > 20) {
+        setErrorFor(password, 'Password must be less than 20 characters');
+        return false;
+    } else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+        return false;
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+        return false;
+	} else{
+		setSuccessFor(password2);
+	}
+    return true;
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control-show error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control-show success';
+}
+
 /*Payment Tab Pane*/
 function openPaymentForm(evt, formName) {
     var x, paymentTabContent, paymentTablinks;
