4a72709c6495ba4572804613031202f5159e6f29
Gyu-Park <kyesung8282@gmail.com>
2021-12-10 17:22:28 -0500

    Update ProductController.java

--- src/main/java/com/groupproject/boogle/controller/ProductController.java
+++ src/main/java/com/groupproject/boogle/controller/ProductController.java
@@ -78,8 +78,10 @@ public class ProductController {
 			Iterator<OrderItem> iter = orderItems.iterator();
 			List<Long> listOfUsersWhoOrderedThisItem = new ArrayList<Long>();
 			while(iter.hasNext()) {
-				if (iter.next().getOrder().getUser().getUserid() != null)
-				listOfUsersWhoOrderedThisItem.add(iter.next().getOrder().getUser().getUserid());
+				OrderItem orderItem = iter.next();
+				if (orderItem.getOrder().getUser().getUserid() != null) {
+					listOfUsersWhoOrderedThisItem.add(orderItem.getOrder().getUser().getUserid());
+				}
 			}
 			if(listOfUsersWhoOrderedThisItem != null) {
 				model.addAttribute("listOfUsersWhoOrderedThisItem", listOfUsersWhoOrderedThisItem);
