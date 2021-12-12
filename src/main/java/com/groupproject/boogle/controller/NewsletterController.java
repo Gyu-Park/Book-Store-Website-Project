94c14e4da24f5f335b6dbbe17fd1221478642ef4
Gyu-Park <kyesung8282@gmail.com>
2021-12-12 11:52:39 -0500

    Update Newsletter Subscription

--- src/main/java/com/groupproject/boogle/controller/NewsletterController.java
+++ src/main/java/com/groupproject/boogle/controller/NewsletterController.java
@@ -4,17 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.groupproject.boogle.service.NewsletterService;
+import com.groupproject.boogle.model.Newsletter;
+import com.groupproject.boogle.repository.NewsletterRepository;
+import com.groupproject.boogle.service.EmailService;
 
 @Controller
 public class NewsletterController {
 	
 	@Autowired
-	NewsletterService newsletterService;
+	NewsletterRepository newsletterRepository;
+	
+	@Autowired
+	private EmailService emailService;
 	
 	@PostMapping("/newsletter")
 	public String addNewsletter (String email) {
-		newsletterService.addEmailforNewsLetterintoDatabase(email);
+		Newsletter newsletter = new Newsletter();
+		newsletter.setEmail(email);
+		newsletterRepository.save(newsletter);
+		emailService.constructNewsletterEmail(newsletter);
 		
 		return "redirect:/home";
 	}
--- src/main/java/com/groupproject/boogle/service/NewsletterService.java
+++ src/main/java/com/groupproject/boogle/service/NewsletterService.java
@@ -12,12 +12,6 @@ public class NewsletterService {
 	@Autowired
 	NewsletterRepository newsletterRepository;
 	
-	public Newsletter addEmailforNewsLetterintoDatabase(String email) {
-		Newsletter newsletter = new Newsletter();
-		newsletter.setEmail(email);
-		return newsletterRepository.save(newsletter);
-	}
-	
 	public Newsletter findNewsletterByemail(String email) {
 		return newsletterRepository.findNewsletterByEmail(email);
 	}
